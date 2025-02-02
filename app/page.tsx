204af1aaec686e9705147ebd4d19011577d6f678
Victor <victorhugo.br2013@gmail.com>
2025-02-02 03:16:37 -0300

    feat: add bia

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import QueryProcessor from "@/components/QueryProcessor";
+import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/sonner";
-import { LucideGithub } from "lucide-react";
+import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <header className="mb-8 border-b pb-2 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">SankhyaQL</h1>
-        <a
-          href="https://github.com/victorhugobraga/inquery"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LucideGithub />
-        </a>
+    <main className="py-4 px-8">
+      <header className="container mx-auto mb-8 border-b pb-2 flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-[#008561]">SankhyaQL</h1>
+        <Button variant={"outline"}>
+          <Plus /> Adicionar Log
+        </Button>
       </header>
-      <QueryProcessor />
+
+      <section className="flex gap-8">
+        <QueryProcessor />
+      </section>
       <Toaster richColors position="top-right" />
     </main>
   );
--- components/QueryProcessor.tsx
+++ components/QueryProcessor.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/card";
 import { Editor } from "@monaco-editor/react";
 import { sendGAEvent } from "@next/third-parties/google";
-import { Clipboard, Plus } from "lucide-react";
+import { ArrowRight, Clipboard, Stars } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { format } from "sql-formatter";
@@ -30,7 +30,7 @@ import {
   TableRow,
 } from "./ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import { Avatar, AvatarFallback } from "./ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { ScrollArea } from "./ui/scroll-area";
 
 type ParamList = {
@@ -116,15 +116,7 @@ export default function QueryProcessor() {
   }, [query]);
 
   return (
-    <Card className="w-full max-w-4xl mx-auto">
-      <CardHeader>
-        <CardTitle className="flex justify-between items-center">
-          <h1>Processador SQL Query</h1>
-          <Button variant={"outline"}>
-            <Plus /> Adicionar Log
-          </Button>
-        </CardTitle>
-      </CardHeader>
+    <Card className="w-full max-w-4xl mx-auto pt-6">
       <CardContent>
         <form>
           <div className="space-y-4">
@@ -175,7 +167,9 @@ export default function QueryProcessor() {
                 <TabsList className="grid w-full grid-cols-3">
                   <TabsTrigger value="result">Resultado</TabsTrigger>
                   <TabsTrigger value="params">Parâmetros</TabsTrigger>
-                  <TabsTrigger value="ia">IA</TabsTrigger>
+                  <TabsTrigger value="ia" className="relative">
+                    BIA <Stars className="w-4 absolute right-1/4" />
+                  </TabsTrigger>
                 </TabsList>
                 <TabsContent value="result">
                   <Editor
@@ -259,57 +253,180 @@ export default function QueryProcessor() {
                 </TabsContent>
                 <TabsContent value="ia">
                   {/* add a mocked template to an response from the ia about what the sql do with an avatar of the ia as an user*/}
-                  <ScrollArea className="h-[30vh] mt-4 pr-3">
+                  <ScrollArea className="h-[40vh] mt-4 pr-3">
                     <div className="flex gap-2">
                       <Avatar>
                         <AvatarFallback>IA</AvatarFallback>
+                        <AvatarImage src="/bia.svg" alt="IA" />
                       </Avatar>
-                      <p className="text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Delectus velit alias ad accusamus quia. Quaerat
-                        eaque iste soluta aliquam voluptate consequuntur totam
-                        porro, cumque debitis. Possimus quo aliquam illum
-                        eligendi? Laudantium nihil perspiciatis necessitatibus
-                        labore laborum inventore sunt esse numquam iusto
-                        voluptatibus amet corrupti quisquam, velit nisi, libero
-                        at expedita. Voluptatibus amet nisi officiis,
-                        exercitationem aperiam sint illo possimus ut? Eveniet
-                        delectus repellat facilis animi. Quisquam minus tempore
-                        placeat laborum nisi doloribus modi sed illum
-                        necessitatibus optio consectetur quidem consequuntur
-                        nesciunt quas totam, quae, sit mollitia. Sit magni
-                        assumenda temporibus. Culpa, quos quis ipsum quaerat
-                        libero sapiente recusandae porro incidunt nobis ad modi.
-                        A, soluta hic eius enim in amet nesciunt officia
-                        laudantium placeat natus, eaque sunt ipsa, porro nulla.
-                        Aliquid autem consequatur nam? Deserunt sint itaque cum
-                        officiis rem beatae voluptates ullam, amet quis
-                        quibusdam nam voluptas temporibus eum omnis facere
-                        consectetur maxime numquam tempora reprehenderit modi
-                        molestias doloremque. Possimus ullam delectus
-                        consectetur voluptatem incidunt dolores similique velit
-                        est aperiam debitis, laboriosam, odio enim adipisci
-                        iusto? Numquam officiis facilis ea unde officia labore
-                        eveniet hic? Eos repellat libero esse? Ipsum, asperiores
-                        ab? Magnam molestiae esse numquam blanditiis possimus
-                        quam incidunt cum reprehenderit magni tenetur,
-                        distinctio omnis consectetur perspiciatis optio quo ut
-                        maiores beatae error quaerat repellat. Molestiae, neque
-                        hic? Unde molestiae hic ut enim ducimus earum sed
-                        reiciendis illum dicta distinctio voluptatum est
-                        temporibus, eveniet autem mollitia porro error voluptate
-                        quas odio voluptas laudantium molestias recusandae! Eum,
-                        aperiam tempora. Quod, hic suscipit, ex ducimus
-                        assumenda porro veniam vitae repellendus blanditiis
-                        eaque provident optio maiores consequatur facilis amet
-                        fugiat officia enim! Suscipit doloribus consequuntur
-                        explicabo praesentium quis earum optio quia! Ab
-                        voluptates maiores eaque hic eos facilis odit quis
-                        itaque? Laborum delectus ipsum quaerat explicabo
-                        obcaecati voluptas eligendi dolore incidunt,
-                        perspiciatis assumenda ipsam temporibus at est, harum
-                        iste quidem distinctio!
-                      </p>
+                      <div className="space-y-2">
+                        <p className="text-sm text-gray-600">
+                          Com base no contexto fornecido, posso explicar que
+                          esta query está realizando uma consulta na tabela
+                          TGFITE (tabela de itens de notas) do sistema Sankhya,
+                          buscando todos os registros (*) onde o NUNOTA (número
+                          único da nota) seja igual a 6581.
+                        </p>
+                        <p className="text-sm text-gray-600">
+                          A tabela TGFITE contém os itens/produtos das notas
+                          fiscais, e esta consulta irá retornar todas as
+                          informações relacionadas aos itens desta nota
+                          específica, incluindo:
+                        </p>
+                        <ul className="text-sm text-gray-600 list-disc pl-4">
+                          <li className="list-item">Produtos (CODPROD)</li>
+                          <li className="list-item">
+                            Quantidades negociadas (QTDNEG)
+                          </li>
+                          <li className="list-item">
+                            Local de origem (CODLOCALORIG)
+                          </li>
+                          <li className="list-item">Controle (CONTROLE)</li>
+                          <li className="list-item">
+                            Status da nota (STATUSNOTA)
+                          </li>
+                          <li className="list-item">
+                            Entre outras informações relacionadas aos itens
+                          </li>
+                        </ul>
+                        <p className="text-sm text-gray-600">
+                          É importante observar que esta consulta mostra apenas
+                          os itens, sem considerar as informações do cabeçalho
+                          da nota que estão armazenadas na tabela TGFCAB. Para
+                          uma análise mais completa, seria interessante
+                          relacionar também com a tabela de cabeçalho através de
+                          um JOIN.
+                        </p>
+                        <p className="text-sm text-gray-600">
+                          Uma versão mais completa da consulta poderia ser:
+                        </p>
+                        <Editor
+                          theme="vs-light"
+                          height="120px"
+                          value={
+                            "SELECT CAB.*, ITE.* \rFROM TGFCAB CAB \rINNER JOIN TGFITE ITE ON CAB.NUNOTA = ITE.NUNOTA \rWHERE CAB.NUNOTA = 6581"
+                          }
+                          onChange={() => setResult(result)}
+                          defaultLanguage="sql"
+                          className="mt-2 border border-gray-300 py-4 overflow-hidden rounded-lg"
+                        />
+                        <p className="text-sm text-gray-600">
+                          Esta versão traria tanto as informações dos itens
+                          quanto do cabeçalho da nota.
+                        </p>
+                        <div className="w-full flex flex-wrap items-center justify-between">
+                          <div className="flex flex-wrap">
+                            <button
+                              className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary-400 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 hover:text-tertiary-hover-foreground focus-visible:text-tertiary-hover-foreground rounded-lg h-fit px-2 py-2"
+                              data-action-id="answer_feedback_positive_btn"
+                              title="Reforço Positivo"
+                              type="button"
+                              aria-haspopup="dialog"
+                              aria-expanded="false"
+                              aria-controls="radix-:r2:"
+                              data-state="closed"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="var(--text-primary-700)"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="lucide lucide-thumbs-up h-4 w-4"
+                              >
+                                <path d="M7 10v12"></path>
+                                <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"></path>
+                              </svg>
+                            </button>
+                            <button
+                              className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary-400 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 hover:text-tertiary-hover-foreground focus-visible:text-tertiary-hover-foreground rounded-lg h-fit px-2 py-2"
+                              data-action-id="answer_feedback_negative_btn"
+                              title="Reforço Negativo"
+                              type="button"
+                              aria-haspopup="dialog"
+                              aria-expanded="false"
+                              aria-controls="radix-:r5:"
+                              data-state="closed"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="transparent"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="lucide lucide-thumbs-down h-4 w-4"
+                              >
+                                <path d="M17 14V2"></path>
+                                <path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a3.13 3.13 0 0 1-3-3.88Z"></path>
+                              </svg>
+                            </button>
+                            <button
+                              className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary-400 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 hover:text-tertiary-hover-foreground focus-visible:text-tertiary-hover-foreground rounded-lg h-fit px-2 py-2"
+                              data-action-id="answer_references_btn"
+                              title="Ver referências"
+                              type="button"
+                              aria-haspopup="dialog"
+                              aria-expanded="false"
+                              aria-controls="radix-:r8:"
+                              data-state="closed"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="lucide lucide-globe h-4 w-4"
+                              >
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
+                                <path d="M2 12h20"></path>
+                              </svg>
+                            </button>
+                            <button
+                              className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary-400 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 hover:text-tertiary-hover-foreground focus-visible:text-tertiary-hover-foreground rounded-lg h-fit px-2 py-2"
+                              data-action-id="answer_retry_btn"
+                              title="Refazer resposta"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="lucide lucide-refresh-cw h-4 w-4"
+                              >
+                                <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"></path>
+                                <path d="M21 3v5h-5"></path>
+                                <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"></path>
+                                <path d="M8 16H3v5"></path>
+                              </svg>
+                            </button>
+                          </div>
+                          <div>
+                            {/* Continue chat link */}
+                            <Button variant={"link"} className="text-[#008561]">
+                              Continuar chat <ArrowRight />
+                            </Button>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </ScrollArea>
                 </TabsContent>
